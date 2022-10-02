73595bf97fb33f91097d8632831843c6bcff896e
Adrien <adrien.schmidt7@gmail.com>
2022-10-02 20:52:52 +0200

    FullPage.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
-function App() {
-  return (
-    <div>React Full page Scrolling</div>
-  );
-}
-export default App;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
+
+const Fullpage = () => (
+  <ReactFullpage
+    //fullpage options
+
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
